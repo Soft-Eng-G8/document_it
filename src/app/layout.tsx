99fadb7c3f67f7f0d9fdf24a5cacca241f81136e
almajd3713 <almajd3713@gmail.com>
2024-11-26 18:42:15 +0100

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default ({children}: {children: React.ReactNode}) => (
+  <html>
+    <body>
+      {/* Nav Components, etc */}
+      {children}
+    </body>
+  </html>
+)
