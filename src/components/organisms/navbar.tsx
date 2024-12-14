1f9fdbc814413275443e49093cf9f648dc772c57
almajd3713 <almajd3713@gmail.com>
2024-12-14 23:49:38 +0100

    add logo to navbar

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import SearchBar from "../molecules/searchBar";
 import { CircleUser, FileText, LogIn, LogOut, Pencil, UserPlus } from "lucide-react";
 
@@ -6,7 +7,12 @@ const Navbar = () => (
   <nav className="shadow-md h-[4rem] bg-mywhite">
     <div className="py-2 px-5 w-full h-full">
       <div className="flex justify-between">
+        <div className="w-full flex">
+          <Link href={"/"} className="flex justify-center items-center">
+            <Image src='/logo.png' width={96} height={40} alt="logo" className="mr-5"/>
+          </Link>
         <SearchBar />
+        </div>
         <div className="flex gap-1 items-center w-1/6 justify-evenly">
           <Link href="/categories">
             <FileText size={32}/>
