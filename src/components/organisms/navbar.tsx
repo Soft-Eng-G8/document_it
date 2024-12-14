e7abe8b53aac89593a52dab59b9ac0c7236fbab5
MHND09 <131745373+MHND09@users.noreply.github.com>
2024-12-14 23:56:13 +0100

    signIn auth

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 import SearchBar from "../molecules/searchBar";
 import { CircleUser, FileText, LogIn, LogOut, Pencil, UserPlus } from "lucide-react";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => (
+
+const Navbar = async () => {
+
+  const {isAuthenticated, getUser} = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  const user = await getUser();
+  return(
   <nav className="shadow-md h-[4rem] bg-mywhite">
     <div className="py-2 px-5 w-full h-full">
       <div className="flex justify-between">
@@ -17,20 +25,27 @@ const Navbar = () => (
           <Link href="/doc_create" title="Create a Document"> 
             <Pencil size={32}/>
           </Link>
-          <Link href='/login' title="Log In">
-            <LogIn size={32} />
-          </Link>
-          <Link href='/signup' title="Sign Up">
-            <UserPlus size={32}/>
-          </Link>
-          <Link href='/logout' title="Log Out">
+          {
+            isUserAuthenticated ? <>
+            <span>{user?.given_name}</span>
+          <LogoutLink title="Log Out">
             <LogOut size={32}/>
-          </Link>
+          </LogoutLink>
+            </>
+            :<>
+          <LoginLink title="Login">
+            <LogIn size={32} />
+          </LoginLink>
+          <RegisterLink title="Sign-up">
+            <UserPlus size={32}/>
+          </RegisterLink>
+            </>
+          }
         </div>
       </div>
     </div>
   </nav>
-);
+);}
 
 
 
