70083f5130df2fd623b3315561b2e3f02bd8d9ca
almajd3713 <almajd3713@gmail.com>
2024-12-26 10:54:21 +0100

    remove signs of bad kinde implementations (no offense mohaned)

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -2,15 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import SearchBar from "../molecules/searchBar";
 import { CircleUser, FileText, LogIn, LogOut, Pencil, UserPlus } from "lucide-react";
-import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
 
 const Navbar = async () => {
 
-  const {isAuthenticated, getUser} = getKindeServerSession();
-  const isUserAuthenticated = await isAuthenticated();
-  const user = await getUser();
+
   return(
   <nav className="shadow-md h-[4rem] bg-mywhite">
     <div className="py-2 px-5 w-full h-full">
@@ -29,22 +25,12 @@ const Navbar = async () => {
           <Link href="/doc_create" title="Create a Document"> 
             <Pencil size={32}/>
           </Link>
-          {
-            isUserAuthenticated ? <>
-            <span>{user?.given_name}</span>
-          <LogoutLink title="Log Out">
+          <Link href="/login" title="Login"> 
+            <LogIn size={32}/>
+          </Link>
+          <Link href="/logout" title="Logout"> 
             <LogOut size={32}/>
-          </LogoutLink>
-            </>
-            :<>
-          <LoginLink title="Login">
-            <LogIn size={32} />
-          </LoginLink>
-          <RegisterLink title="Sign-up">
-            <UserPlus size={32}/>
-          </RegisterLink>
-            </>
-          }
+          </Link>
         </div>
       </div>
     </div>
