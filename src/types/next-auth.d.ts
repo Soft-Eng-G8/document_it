cc949aa82dcdfd80ce5a0ac95840f4da8012ced9
almajd3713 <almajd3713@gmail.com>
2024-12-26 17:19:55 +0100

    add custom auth types

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { User as PrismaUser } from "@prisma/client";
+
+// extending to add roles
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string
+      email: string
+      roles: {
+        id: string, name: string
+      }[]
+    } & DefaultSession['user']
+  }
+
+  interface JWT {
+    id: string
+    email: string
+    roles: {
+      id: string, name: string
+    }[]
+  }
+
+  interface User extends PrismaUser {
+    roles: {
+      id: string, name: string
+    }[]
+  }
+}
